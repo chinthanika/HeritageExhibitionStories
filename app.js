db815cf18b06696ed8beab92294364ebb57c2d14
Chinthanika Iddamalgoda <85867263+chinthanika@users.noreply.github.com>
2023-07-21 02:54:33 +0800

    - Added save as PDF function

--- app.js
+++ app.js
@@ -28,8 +28,40 @@ document.addEventListener('DOMContentLoaded', () => {
         const storyText = await generateStory(prompt);
         document.getElementById('generated-story').innerText = storyText;
     });
-
 });
+
+
+function saveAsPDF() {
+    var HTML_Width = $(".story").width();
+    var HTML_Height = $(".story").height();
+    var top_left_margin = 15;
+    var PDF_Width = HTML_Width + (top_left_margin * 2);
+    var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+    var canvas_image_width = HTML_Width;
+    var canvas_image_height = HTML_Height;
+
+    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+
+    html2canvas($(".story")[0], { allowTaint: true }).then(function (canvas) {
+        canvas.getContext('2d');
+
+        console.log(canvas.height + "  " + canvas.width);
+
+
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+
+
+        for (var i = 1; i <= totalPDFPages; i++) {
+            pdf.addPage(PDF_Width, PDF_Height);
+            pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+        }
+
+        pdf.save("HTML-Document.pdf");
+    });
+}
 
 // Function to call the OpenAI API
 async function generateStory(prompt) {
@@ -39,7 +71,7 @@ async function generateStory(prompt) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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'
+            'Authorization': 'Bearer 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'
         },
         body: JSON.stringify({
             model: 'text-davinci-003',
